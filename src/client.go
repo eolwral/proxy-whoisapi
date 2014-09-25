f432876443bd6f88c283d15ba1432418b4a144fb
eolwral <eolwral@gmail.com>
2014-09-26 03:25:51 +0800

    fix empty location issue, set lat and lon as float64

--- src/client.go
+++ src/client.go
@@ -18,19 +18,20 @@ const (
  * QueryResponse Json Object
  **/
 type QueryReponse struct {
+	AS          string `json:"as"`
+	City        string `json:"city"`
 	Country     string `json:"country"`
 	CountryCode string `json:"countryCode"`
+	ISP         string `json:"isp"`
+	Latitude    float64 `json:"lat"`
+	Longitude   float64 `json:"lon"`
+	ORG         string `json:"org"`
+	IP          string `json:"query"`
 	Region      string `json:"region"`
 	RegionName  string `json:"regionName"`
-	City        string `json:"city"`
-	Zip         string `json:"zip"`
+        Status      string `json:"status"`
 	Timezone    string `json:"timezone"`
-	IP          string `json:"query"`
-	AS          string `json:"as"`
-	ISP         string `json:"isp"`
-	ORG         string `json:"org"`
-	Longitude   string `json:"lon"`
-	Latitude    string `json:"lat"`
+	Zip         string `json:"zip"`
 }
 
 /**
